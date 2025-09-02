1d271550e4d387cc7f101ba26519dddbb5d6181c
Harry De Torres <hdetorres1134@gmail.com>
2025-09-02 13:48:33 +0800

    a

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
     if(!isset($_SESSION['user_id'])){
         header("Location: login.php");
     }
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
 ?>
 <!DOCTYPE html>
 <html lang="en">
